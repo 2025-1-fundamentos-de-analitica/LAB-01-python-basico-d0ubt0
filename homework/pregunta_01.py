bb4cd782e33fd96796e6c2aad01dca549ab87033
d0ubt <sepabonn@unal.edu.co>
2025-05-09 16:57:42 -0500

    Laboratorio 1 v1.0

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,12 +5,18 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
 
     Rta/
     214
-
     """
+    with open('files/input/data.csv', 'r') as file:
+        suma = 0
+        for linea in file:
+            datos = linea.split()
+            suma += int(datos[1])
+
+        return suma
+pregunta_01()
